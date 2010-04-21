0cb58b54d902324a73cd55ada71529b38621155f
Eun-seo Choi <echoi@ig.utexas.edu>
2010-04-21 19:16:50 +0000

    Reverting coord files to initCoord files: Otherwise, bottomRestore doesn't work as intended.

--- Snac/plugins/remesherSPR/InitialConditions.c
+++ Snac/plugins/remesherSPR/InitialConditions.c
@@ -82,7 +82,7 @@ void _SnacRemesher_InitialConditions( void* _context, void* data ) {
 		char path[PATH_MAX];
 		double	x,y,z;
 
-		sprintf(path, "%s/snac.coord.%d.%06d.restart",context->outputPath,context->rank,context->restartTimestep);
+		sprintf(path, "%s/snac.initCoord.%d.%06d.restart",context->outputPath,context->rank,context->restartTimestep);
 		Journal_Firewall( ( (fp=fopen(path,"r")) != NULL), context->snacError, "Can't find %s", path );
 		for( lNode_i = 0; lNode_i < mesh->nodeLocalCount; lNode_i++ ) {
 			fscanf( fp, "%le %le %le", &x,&y,&z);
