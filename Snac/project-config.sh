322273671706e5da368ce383f1317765f08c39f0
Eun-seo Choi <echoi@ig.utexas.edu>
2012-03-12 21:59:06 +0000

    GSL not needed any more - don't do the GSL configuration. Barycentric Interpolation (BI) is now the remeshing scheme of choice.

--- Snac/project-config.sh
+++ Snac/project-config.sh
@@ -53,6 +53,6 @@ setValue PROJECT 'Snac'
 . ./VMake/Config/mpi-config.sh
 . ./VMake/Config/xml-config.sh
 . ./VMake/Config/StGermain-config.sh
-. ./VMake/Config/gsl-config.sh
+## . ./VMake/Config/gsl-config.sh
 ## . ./VMake/Config/python-config.sh --optional
 
